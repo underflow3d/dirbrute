dc438715dec73ec99a652865cbf79e260360c6fb
Pwn3r Security <developer6383@gmail.com>
2023-08-23 18:50:36 +0500

    Optimized code

--- dirbrute.py
+++ dirbrute.py
@@ -4,24 +4,24 @@ import argparse
 valid_status_codes = [200, 301]
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-u', '--url', type=str, required=True)
-parser.add_argument('-w', '--wordlist', type=str, required=True)
+parser.add_argument("-u", "--url", type=str, required=True)
+parser.add_argument("-w", "--wordlist", type=str, required=True)
 args = parser.parse_args()
 
-wordlist = open(args.wordlist, 'r')
+wordlist = open(args.wordlist, "r")
 
 while True:
-    page = wordlist.readline().replace('\n', '')
-    if page == '':
+    word = wordlist.readline().replace("\n", "")
+    if word == "":
         break
     else:
-        m_url = args.url + page
-        resp = requests.get(m_url)
+        url = args.url + word
+        resp = requests.get(url)
         if resp.status_code in valid_status_codes:
-            print(f'Found: {m_url}')
+            print(f"Found: {url}")
         else:
             continue
 
 wordlist.close()
 
-print('Attack finished!')
+print("Attack finished!")
