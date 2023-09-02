1332ceb93295607c244080b54a0f3b9baaa677df
Ivan L <ivlabush@gmail.com>
2023-09-03 00:00:42 +0300

    Improvements list: 1. Simplify if statement 2. Use with-as syntax to avoid manual close 3. Rewrite mani section on for loop 4. Extend output by status code

--- dirbrute.py
+++ dirbrute.py
@@ -8,23 +8,17 @@ parser.add_argument("-u", "--url", type=str, required=True)
 parser.add_argument("-w", "--wordlist", type=str, required=True)
 args = parser.parse_args()
 
-if args.url.endswith("/") == False:
+if not args.url.endswith("/"):
     args.url = args.url + "/"
 
-wordlist = open(args.wordlist, "r")
-
-while True:
-    word = wordlist.readline().strip()
-    if not word: 
-        break
-    else:
+with open(args.wordlist, "r") as wordlist:
+    for word in wordlist:
         url = args.url + word
         resp = requests.get(url)
         if resp.status_code in valid_status_codes:
-            print(f"Found: {url}")
+            print(f"Found: {url}, status code {resp.status_code}")
         else:
             continue
 
-wordlist.close()
 
 print("Attack finished!")
