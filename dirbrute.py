681caf9920c960c978d677b33f3b936791d95042
Ivan L <ivlabush@gmail.com>
2023-09-07 13:45:25 +0300

    Improve help

--- dirbrute.py
+++ dirbrute.py
@@ -3,9 +3,12 @@ import argparse
 
 valid_status_codes = [200, 301]
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-u", "--url", type=str, required=True)
-parser.add_argument("-w", "--wordlist", type=str, required=True)
+parser = argparse.ArgumentParser(
+    prog="python3 dirbrute.py",
+    description="A lightweight and easy to use directory buster written in Python3."
+)
+parser.add_argument("-u", "--url", type=str, required=True, help="URL should be tested started with http(s)://")
+parser.add_argument("-w", "--wordlist", type=str, required=True, help="Location of wordlist")
 args = parser.parse_args()
 
 if not args.url.endswith("/"):
